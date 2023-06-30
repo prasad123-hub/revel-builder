f9e04a35582ad9a0f24df6fd198b98742dd13f0f
prasad123-hub <narkhedkarprasad@gmail.com>
2023-06-30 08:56:55 +0530

    font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter, Caveat } from "next/font/google";
+import LocalFont from "next/font/local";
+
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const fontHand = Caveat({ subsets: ["latin"], variable: "--font-hand" });
+
+// import calsans font from /src/styles/fonts/calsans.ttf
+const fontCal = LocalFont({
+  src: "../styles/fonts/calsans.ttf",
+  variable: "--font-cal",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={cn(
+        "min-h-screen font-sans antialiased",
+        fontSans.variable,
+        fontHand.variable,
+        fontCal.variable
+      )}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
