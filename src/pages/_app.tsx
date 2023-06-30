afbcc5db57f9a9593375537abbeda9216b48ff82
prasad123-hub <narkhedkarprasad@gmail.com>
2023-06-30 09:06:07 +0530

    eslint and prettier configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
-import { cn } from "@/lib/utils";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Inter, Caveat } from "next/font/google";
-import LocalFont from "next/font/local";
+import { cn } from "@/lib/utils"
 
-const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
-const fontHand = Caveat({ subsets: ["latin"], variable: "--font-hand" });
+import "@/styles/globals.css"
+
+import type { AppProps } from "next/app"
+import { Caveat, Inter } from "next/font/google"
+import LocalFont from "next/font/local"
+
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" })
+const fontHand = Caveat({ subsets: ["latin"], variable: "--font-hand" })
 
 // import calsans font from /src/styles/fonts/calsans.ttf
 const fontCal = LocalFont({
   src: "../styles/fonts/calsans.ttf",
   variable: "--font-cal",
-});
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -25,5 +27,5 @@ export default function App({ Component, pageProps }: AppProps) {
     >
       <Component {...pageProps} />
     </main>
-  );
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
-
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-cal">
+      <h1 className="font-cal text-4xl font-bold">
         Collect Testimonials in no time
       </h1>
-      <p className="text-lg font-medium font-hand">
+      <p className="font-hand text-lg font-medium">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
       </p>
       <Button size="lg">Get Started</Button>
     </div>
-  );
+  )
 }
